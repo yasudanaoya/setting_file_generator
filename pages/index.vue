611e741a173f1e3d67e52039973d3eecc2cb5e9d
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2020-06-10 00:36:35 +0900

    feat : delete un need code

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,9 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        app
-      </h1>
-      <h2 class="subtitle">
-        My bee&#39;s knees Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  .page-top
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
+export default {};
 </script>
 
 <style>
