381e471ce464737bfd739c151c7cf224a1daf7ac
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2020-06-10 23:25:09 +0900

    feat : ability table base

---
+++ assets/mixins/ability-list.js
@@ -0,0 +1,12 @@
+export default {
+  methods: {
+    getAbilities() {
+      return [
+        {
+          key: "whiteSpace",
+          desc: "保存時に、文末の無駄なスペースを削除する"
+        }
+      ];
+    }
+  }
+};
---
+++ components/molecules/AbilityRow.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  tr
+    td
+    td
+      input(
+        type="checkbox"
+        @change="onChange"
+      )
+    td {{ getDescription }}
+
+</template>
+
+<script>
+import abilityList from "@/assets/mixins/ability-list";
+
+export default {
+  mixins: [abilityList],
+
+  props: {
+    ability: {
+      type: Object,
+      required: true
+    },
+
+    index: {
+      type: Number,
+      required: true
+    },
+
+    checkedVal: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    getDescription() {
+      return this.getAbilities().find(value => value.key === this.ability.kind)
+        .desc;
+    }
+  },
+
+  methods: {
+    onChange() {}
+  }
+};
+</script>
