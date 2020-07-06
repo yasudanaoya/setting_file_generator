4dac81462983279df95d353290022d7c0613ab45
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2020-07-07 03:10:42 +0900

    feat : install vue-dropdown-menu

--- components/molecules/SwitchLanguage.vue
+++ components/molecules/SwitchLanguage.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
   .switch-language
-    button(
-      @click.self="onClick"
+    dropdown-menu(
+      v-model="isShow"
+      transition="translate-fade-down"
     )
-      | {{ $t("common.button.translations") }}
+      button {{ $t("common.button.translations") }}
 
-    .language-list(
-      v-if="isShow"
-    )
-      .language-item(
-        v-for="language in languages"
-      )
-        nuxt-link(:to="switchLocalePath(language.lang)")
-          | {{ language.name }}
+      .language-list(slot="dropdown")
+        .language-item(
+          v-for="language in languages"
+        )
+          nuxt-link(:to="switchLocalePath(language.lang)")
+            | {{ language.name }}
 </template>
 
 <script>
@@ -25,12 +24,82 @@ export default {
         { lang: "en", name: "English" }
       ]
     };
-  },
-
-  methods: {
-    onClick() {
-      this.isShow = !this.isShow;
-    }
   }
 };
 </script>
+
+<style lang="scss">
+.switch-language {
+  button {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    vertical-align: middle;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+
+    &::after {
+      display: inline-block;
+      margin-left: 0.255em;
+      vertical-align: 0.255em;
+      content: "";
+      border-top: 0.3em solid;
+      border-right: 0.3em solid transparent;
+      border-bottom: 0;
+      border-left: 0.3em solid transparent;
+    }
+  }
+
+  .language-list {
+    font-size: 1rem;
+    color: #212529;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    border-radius: 0.25rem;
+    z-index: 1000;
+
+    .language-item {
+      display: block;
+      padding: 0.25rem 1.5rem;
+      clear: both;
+      font-weight: 400;
+      text-align: inherit;
+      white-space: nowrap;
+      border: 0;
+
+      &:hover {
+        background-color: #f8f9fa;
+      }
+    }
+  }
+
+  /* translate fade (top to down) */
+  .translate-fade-down-enter-active,
+  .translate-fade-down-leave-active {
+    transition: all 250ms;
+    transition-timing-function: cubic-bezier(0.53, 2, 0.36, 0.85);
+  }
+
+  .translate-fade-down-enter,
+  .translate-fade-down-leave-active {
+    opacity: 0;
+  }
+
+  .translate-fade-down-enter,
+  .translate-fade-down-leave-to {
+    position: absolute;
+  }
+
+  .translate-fade-down-enter {
+    transform: translateY(-10px);
+  }
+
+  .translate-fade-down-leave-active {
+    transform: translateY(10px);
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ["plugins/prism", "plugins/modal"],
+  plugins: ["plugins/prism", "plugins/modal", "plugins/dropdown"],
   /*
    ** Nuxt.js dev-modules
    */
