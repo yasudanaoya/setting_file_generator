30ce1f2db66aa9655785b8089ded53016957c552
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2020-07-09 02:25:00 +0900

    feat : footer base

---
+++ components/atoms/Copyright.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .copyright
+    | Copyright © 2020
+    a.github-link(href="https://github.com/yasudanaoya") yasudanaoya
+    | . All rights reserved.
+</template>
+
+<style lang="scss" scoped>
+.copyright {
+  a.github-link {
+    text-decoration: underline;
+    margin: 0 1vw;
+
+    &:visited {
+      color: #fff;
+    }
+  }
+}
+</style>
